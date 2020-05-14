e53a07ce476d2fb7552e84eff210091d9bc321e2
halki <s.halki0203@icloud.com>
2020-05-14 23:10:48 +0900

    update

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,16 +7,16 @@ class CategoriesController < ApplicationController
     def show
       category = Category.find_by(id: params[:id])
       # # 親カテゴリー選択時
-      # if category.ancestry == nil
-      #   @category_items = Category.find_by(id: params[:id]).indirect_ids
+      if category.ancestry == nil
+        @category_items = Category.find_by(id: params[:id]).indirect_ids
       # # 子カテゴリー選択時
 
 
       # 孫カテゴリー選択時
-      # elsif category.ancestry.include?("/")
+      elsif category.ancestry.include?("/")
         @category_items = Item.where(category_id: params[:id])
 
-      # end
+      end
 
     end
 
